99dd058ca94cb2195b1c7ad0d8088463aee50fca
shift-d <shiftkeydeveloper@gmail.com>
2022-11-06 00:09:01 +0300

    [parser]: word

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,12 @@
+use crate::tokenizer::{tokenize, BasicToken, BasicTokenType};
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct ParsedToken {
     range: [u32; 2],
     data: ParsedTokenData,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum ParsedTokenData {
     Word(String),
     Space(u32),
@@ -12,21 +16,25 @@ enum ParsedTokenData {
     AttachedModifier(AttachedModifier),
 }
 
+#[derive(Debug, Eq, PartialEq)]
 struct Link {
     variant: LinkType,
     content: String,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum LinkType {
     Url,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 struct AttachedModifier {
     char: char,
     variant: AttachedModifierType,
     content: Vec<ParsedToken>,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum AttachedModifierType {
     Bold,
     Italic,
@@ -37,3 +45,53 @@ enum AttachedModifierType {
     Comment,
 }
 
+pub fn parse<I>(basic_tokens: I) -> Vec<ParsedToken>
+where
+    I: Iterator<Item = BasicToken>
+{
+    let mut parsed_tokens: Vec<ParsedToken> = vec![];
+    let mut basic_tokens = basic_tokens.peekable();
+
+    while let Some(basic_token) = basic_tokens.next() {
+        match basic_token.token_type {
+            BasicTokenType::Character => {
+                let mut word = String::new();
+                word.push(basic_token.char);
+                loop {
+                    // TODO: break using 1.65 syntax
+                    match basic_tokens.peek() {
+                        Some(next_token) => {
+                            if next_token.token_type == BasicTokenType::Character {
+                                word.push(basic_tokens.next().unwrap().char);
+                            } else {
+                                break
+                            }
+                        },
+                        None => break,
+                    }
+                }
+                parsed_tokens.push(ParsedToken {
+                    range: [0, (word.len() - 1) as u32],
+                    data: ParsedTokenData::Word(word),
+                })
+            }
+            _ => unimplemented!(),
+        }
+    }
+    parsed_tokens
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn word() {
+        assert_eq!(parse(tokenize("hi")), vec![
+                   ParsedToken {
+                       range: [0,1],
+                       data: ParsedTokenData::Word("hi".into()),
+                   }
+        ]);
+    }
+}
